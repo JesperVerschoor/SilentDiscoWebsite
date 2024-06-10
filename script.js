18e758543da1771da574ffdc941f8c4f2a4d6702
Jesper <jesperv90@gmail.com>
2024-06-10 21:04:01 +0200

    - Add reserveer nu call to action buttons on Index and Inspiratie - Add script to hide bekijk winkelwagen knop in booqable shopping cart pop up

--- script.js
+++ script.js
@@ -10,17 +10,45 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             document.getElementById("footer-placeholder").innerHTML = data;
         });
+
+    // Code to hide the Naar winkelwagen button on the Booqable shopping cart 
+    // popup, because that button has a bug on the Booqable side. It is unclear
+    // when this will be resolved by them. Therefore, temporarily hide the button
+    // on page load.
+    const observer = new MutationObserver(function(mutationsList, observer) {
+        mutationsList.forEach(function(mutation) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        const button = node.querySelector('.bq-button[href*="carts"]');
+                        if (button) {
+                            button.style.display = 'none';
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });    
+    // end block
+
+    
+
 });
 
+// Booqable integration
 var booqableOptions = {
     company: "f6757dda-e195-43ca-ae5a-772f4b64db0a",
 };
+
 
 //reserveren information icon
 
 
 //FAQ accordion list
 document.addEventListener('DOMContentLoaded', function() {
+    //FAQ accordion list
     var accordionItems = document.querySelectorAll('.accordion-item');
 
     accordionItems.forEach(function(item) {
