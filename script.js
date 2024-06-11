0e2d2bf77b59da7ad69c9166c5e650b7bfa8c75d
Jesper <jesperv90@gmail.com>
2024-06-11 22:31:35 +0200

    index pagina keihard aangewerkt nog niet af

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             document.getElementById("navbar-placeholder").innerHTML = data;
         });
+    
+       fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
 
     fetch("footer.html")
         .then(response => response.text())
